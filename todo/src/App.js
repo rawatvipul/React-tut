0e7808022d045ee5cecfbe8c87f5fdcd88d26771
vipul rawat <rawatvipul14@gmail.com>
2024-02-26 19:17:52 +0530

    mount

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  constructor()
+  {
+    super();
+    this.state={
+      name:"Vipul"
+    }
+    // console.warn("constructor")
+  }
+  componentDidMount()
+  {
+    console.warn("Componentdidimount")
+  }
+  render()
+  {
+    console.warn("render")
+    return (
+      <div className="App">
+        <h1>Hello {this.state.name}</h1>
+        <button onClick={()=>{this.setState({name:"rawat"})}}>Update</button>
+      </div>
+    );
+  }
 }
 
 export default App;
